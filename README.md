18e3b0be95cdf99559d39aa300fff972dd87cd8f
Dharmveer Baloda <dharmveer.baloda@erosnow.com>
2021-03-23 22:10:40 +0530

    thumbnail sprite correction

--- README.md
+++ README.md
@@ -6,9 +6,17 @@
     2. ImageMagick Montage
 
 # Steps:
-    1. Extract images using ffmpeg. You can configure size of image and image rate per second(ips)
+    1. Extract images using ffmpeg. You can configure size of image and image rate per second(IPS)
     2. Convert Image in spirtesheet of grid ROWSxCOLS
     3. Create a webvtt file of spritesheet images
+
+Recomanded Values for IPS:
+    1
+    0.50: 1 image every 2 seconds)
+    0.25: 1 image in every 4 second
+    0.20: 1 image in every 5 second
+    0.10: 1 image in every 10 second
+    0.05: 1 image in every 20 second
 
 It uses temp folder for storage. for persistence storage move these different folder or location.
 
--- msprites/webvtt.py
+++ msprites/webvtt.py
@@ -4,7 +4,7 @@ from msprites.settings import Settings
 
 class WebVTT:
     HEADER = "WEBVTT\n"
-    TIME_FORMAT = "%H:%M:%S"
+    TIME_FORMAT = "%H:%M:%S.000"
     TIMELINE_FORMAT = "{start} --> {end}\n"
     IMAGE_TITLE_FORMAT = "{filename}#xywh={x},{y},{w},{h}\n\n"
     FILENAME = "sprite.webvtt"
@@ -13,7 +13,10 @@ class WebVTT:
         self.sprites = sprites
         self.dir = sprites.dir
 
-    def ips_seconds_to_timestamp(self, ips):
+    def ips_to_seconds(self):
+        return (1/Settings.IPS)
+
+    def seconds_to_timestamp(self, ips):
         return time.strftime(WebVTT.TIME_FORMAT, time.gmtime(ips))
 
     def getx(self, imnumber, w, h):
@@ -35,15 +38,15 @@ class WebVTT:
 
     def content(self):
         contents = [WebVTT.HEADER]
-        start, end, filename = 0, Settings.IPS, ""
+        start, end, filename = 0, self.ips_to_seconds(), ""
         w, h, gridsize = Settings.WIDTH, Settings.HEIGHT ,Settings.ROWS * Settings.COLS
         for i in range(0, self.sprites.thumbs.count()):
 
             filename = Settings.spritefilename((i+1)//gridsize)
             contents+=[
                 WebVTT.TIMELINE_FORMAT.format(
-                    start=self.ips_seconds_to_timestamp(start),
-                    end=self.ips_seconds_to_timestamp(end),
+                    start=self.seconds_to_timestamp(start),
+                    end=self.seconds_to_timestamp(end),
                 ),
                 WebVTT.IMAGE_TITLE_FORMAT.format(
                     x=self.getx(i, w, h), y=self.gety(i, w, h),
@@ -51,7 +54,7 @@ class WebVTT:
                 )
             ]
             start = end
-            end += Settings.IPS
+            end += self.ips_to_seconds()
         return contents
 
     def dest(self):
